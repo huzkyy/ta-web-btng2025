e53c7e093bfc19232fbf0b4cf655fbb8ed78008d
Huzkyy <hazksmg@gmail.com>
2025-10-25 10:31:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// getElementById()
+const elementNode = document.getElementById("judul");
+const textNode = elementNode.textContent;
+console.log(textNode);
+console.log(elementNode.nodeType);
+
+// getElementsByClassName
+const elements = document.getElementsByClassName("paragraf")[0];
+console.log(elements);
